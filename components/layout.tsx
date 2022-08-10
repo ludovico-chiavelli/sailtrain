71e7e1e7b13bf4dd1238139498287546f3033ebd
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 10:32:30 +0100

    compose layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import type { FC, PropsWithChildren } from 'react';
 
 import Header from './header';
+import Footer from './footer';
 
 const Layout:FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen flex flex-col'>
       <Header />
-      <main>{children}</main>
+      <main className='flex-1 flex justify-center'>
+        <section className='relative mt-16 w-full'>
+          <div className='absolute inset-x-8'>
+            {children}
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   )
 }
