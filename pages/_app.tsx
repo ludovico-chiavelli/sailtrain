49a86cc56fc0f55a2ffed8348da5d98cea90409d
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 08:51:46 +0100

    fix import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import '../styles.css'
+import '../styles/globals.css'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
