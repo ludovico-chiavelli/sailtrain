997081261ebc264b879d05484967a1ce6e23dd89
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 10:32:37 +0100

    create footer

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const Footer:FC = () => {
+  return (
+    <footer>
+      <div className='flex justify-center'>
+        <h1>Made with 🍕 by SailTrain</h1>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
