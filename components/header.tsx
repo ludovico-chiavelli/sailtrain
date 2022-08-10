fb050f75324839ec2c127c2f0aab5264b95afdbf
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 10:31:57 +0100

    control growing of header

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import LogoNav from './logo-nav';
 
 const Header:FC = () => {
   return (
-    <header className='relative'>
+    <header className='flex-initial relative'>
       <div className='bg-gradient-to-r from-slate-800 to-slate-400 h-44 w-full'></div>
       <LogoNav/>
     </header>
