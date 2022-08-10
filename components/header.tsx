9bf24d5fb1020cfb3492e4de1875ef117a29a659
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 09:35:39 +0100

    make full width and height 44rem

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import type { FC } from 'react';
 const Header:FC = () => {
   return (
     <header>
-      <div className='absolute top-0 left-0 bg-gradient-to-r from-slate-800 to-slate-400 h-60'></div>
+      <div className='absolute top-0 left-0 bg-gradient-to-r from-slate-800 to-slate-400 h-44 w-full'></div>
     </header>
   )
 }
