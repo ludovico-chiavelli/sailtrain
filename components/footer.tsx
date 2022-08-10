e5f197fb1b899471fc8536a3ebe7c61b3031ee57
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 10:36:35 +0100

    center footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,8 @@ import type { FC } from 'react';
 
 const Footer:FC = () => {
   return (
-    <footer>
-      <div className='flex justify-center'>
+    <footer className='basis-28'>
+      <div className='flex justify-center items-center'>
         <h1>Made with 🍕 by SailTrain</h1>
       </div>
     </footer>
