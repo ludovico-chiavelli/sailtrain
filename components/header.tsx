30e7f622e1647385445ed9aee24d14e3b5c52966
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-10 09:24:22 +0100

    create header

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const Header:FC = () => {
+  return (
+    <header>
+      <div className='absolute top-0 left-0 bg-gradient-to-r from-slate-800 to-slate-400 h-60'></div>
+    </header>
+  )
+}
+
+export default Header;
