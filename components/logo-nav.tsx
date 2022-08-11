cd6fc3c6507b46b8d29bc942e7a9435037791f93
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-11 09:54:05 +0100

    make conditionally render mobile or desktop layout

--- components/logo-nav.tsx
+++ components/logo-nav.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import type { FC } from 'react';
 
+import { useThemeContext } from './layout';
+
 const LogoNav:FC = () => {
-  return (
-    <div className='absolute -bottom-8 inset-x-0'>
-      <div className='flex justify-center'>
-        <div className='rounded-full h-16 w-72 bg-slate-500 relative'>
-          <div className='absolute -top-10 inset-x-0'>
-            <div className='flex justify-center'>
-              <div className='rounded-full h-24 w-24 bg-slate-700'></div>
+  const isMobile = useThemeContext();
+
+  if (isMobile) {  
+    return (
+      <div className='absolute -bottom-8 inset-x-0'>
+        <div className='flex justify-center'>
+          <div className='rounded-full h-16 w-72 bg-slate-500 relative'>
+            <div className='absolute -top-10 inset-x-0'>
+              <div className='flex justify-center'>
+                <div className='rounded-full h-24 w-24 bg-slate-700'></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className='absolute -top-14 md:left-20 lg:left-36'>
+          <div className='rounded-full h-[512px] w-32 bg-slate-500 relative'>
+            <div className='absolute top-3 inset-x-0'>
+              <div className='flex justify-center'>
+                <div className='rounded-full h-28 w-28 bg-slate-700'></div>
+              </div>
+            </div>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default LogoNav
