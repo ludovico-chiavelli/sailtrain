4368755cef88ae0d0c83748120e5d91c365b6e2a
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-16 11:36:50 +0100

    add links to home and github profile

--- components/logo-nav.tsx
+++ components/logo-nav.tsx
@@ -13,18 +13,20 @@ const LogoNav = ({ Icons }: { Icons: { MarkGithubIcon: Icon, ArchiveIcon: Icon}}
       <div className='absolute -bottom-8 inset-x-0 drop-shadow-md sm:scale-150'>
         <div className='flex justify-center'>
           <div className='rounded-full h-16 w-72 bg-bBeige relative'>
-            <div className='absolute top-2 left-8'>
-              <div className='flex flex-col items-center h-10'>
+            <a className='absolute top-3 left-8 z-10' href='https://github.com/ludovico-chiavelli'>
+              <button className='flex flex-col items-center h-10'>
                 <Icons.MarkGithubIcon size={22}/>
                 <div>GitHub</div>
-              </div>
-            </div>
+              </button>
+            </a>
             <div className='absolute -top-10 inset-x-0'>
-              <div className='flex justify-center'>
-                <div className='rounded-full h-24 w-24 overflow-hidden'>
-                  <Image src={'/pfp.png'} alt='Profile Picture' width={540} height={540} objectFit='cover'/>
-                </div>
-              </div>
+              <Link href='/'>
+                <a className='flex justify-center'>
+                  <div className='rounded-full h-24 w-24 overflow-hidden'>
+                    <Image src={'/pfp.png'} alt='Profile Picture' width={540} height={540} objectFit='cover'/>
+                  </div>
+                </a>
+              </Link>
             </div>
             <div className='absolute top-3 right-8'>
               <Link href='/projects'>
@@ -43,18 +45,22 @@ const LogoNav = ({ Icons }: { Icons: { MarkGithubIcon: Icon, ArchiveIcon: Icon}}
       <div className='absolute -top-4 md:left-20 lg:left-36 drop-shadow-md scale-125'>
         <div className='rounded-full h-[384px] w-32 bg-bBeige relative'>
           <div className='absolute top-3 inset-x-0'>
-            <div className='flex justify-center'>
-              <div className='rounded-full h-28 w-28 overflow-hidden'>
-                <Image src={'/pfp.png'} alt='Profile Picture' width={540} height={540} objectFit='cover'/>
-              </div>
-            </div>
+            <Link href='/'>
+              <a className='flex justify-center'>
+                <div className='rounded-full h-28 w-28 overflow-hidden'>
+                  <Image src={'/pfp.png'} alt='Profile Picture' width={540} height={540} objectFit='cover'/>
+                </div>
+              </a>
+            </Link>
           </div>
           <div className='h-1/2 absolute top-36 w-full'>
             <div className='h-full flex flex-col items-center justify-around'>
-              <div className='flex flex-col items-center h-10'>
-                <Icons.MarkGithubIcon size={30}/>
-                <div>GitHub</div>
-              </div>
+              <a className='z-10' href='https://github.com/ludovico-chiavelli'>
+                <div className='flex flex-col items-center h-10'>
+                  <Icons.MarkGithubIcon size={30}/>
+                  <div>GitHub</div>
+                </div>
+              </a>
               <Link href='/projects'>
                 <a className='flex flex-col items-center h-10'>
                   <Icons.ArchiveIcon size={30}/>
